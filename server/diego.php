20ecd648710acea68eb3953c21c48c08c0c8fd90
Gabriel Joulié <gabriel@gaggou.com>
2015-06-13 16:45:21 +0200

    remove test function

--- server/diego.php
+++ server/diego.php
@@ -9,19 +9,13 @@ function diego_act(){
   $id = intval($_POST["id"]);
   $state = $_POST["state"];
   if ($state == "ON"){
-    $return_val = exec("tdtool -f " . $id, $table, $status);
-  } else {
     $return_val = exec("tdtool -n " . $id, $table, $status);
+  } else {
+    $return_val = exec("tdtool -f " . $id, $table, $status);
   }
   $return["status"] = $status;
   $return["string"] = $return_val;
   $return["id"] = $id;
-  return $return;
-}
-function test_function(){
-  $return = $_POST;
-  
-  $return["json"] = json_encode($return);
   return $return;
 }
 
@@ -41,20 +35,26 @@ if (is_ajax()) {
   if (isset($_POST["action"]) && !empty($_POST["action"])) { //Checks if action value exists
     $action = $_POST["action"];
     switch($action) { //Switch case for value of action
-      case "test": echo json_encode(test_function()); break;
       case "act":  echo json_encode(diego_act()); break;
       case "get":  echo json_encode(get_status()); break;
     }
     exit;
   }
+  print "Ajax but no action\n";
+  exit;
 }
 ?>
 <head>
-<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="jquery-1.11.0.min.js"></script>
 <!--Put the following in the <head>-->
 <script type="text/javascript">
 $("document").ready(function(){
   $(".setter").click(function(){
+    if($(this).attr("state") == "ON"){
+      $(this).attr("state", "OFF");
+    } else {
+      $(this).attr("state", "ON");
+    }
     var data = {
       "action": "act",
       "id": $(this).attr("id"),
@@ -70,12 +70,6 @@ $("document").ready(function(){
         $(".the-return").html(
           "Command done: " + data["string"] + "<br />status: " + data["status"] + "<br />"
         );
-        var target = "#" + data["id"];
-        if($(target).attr("state") == "ON"){
-          $(target).attr("state", "OFF");
-        } else {
-          $(target).attr("state", "ON");
-        }
       }
     });
     return false;
@@ -105,13 +99,15 @@ $("document").ready(function(){
 <body>
 <?php
   foreach(get_status() as $binou){
-    echo "<button id=\"${binou["id"]}\" state=\"${binou["status"]}\" class=\"setter\">${binou["identifier"]}</button></br>";
+    echo "
+<button id=\"${binou["id"]}\" state=\"${binou["status"]}\" class=\"setter\">${binou["identifier"]}</button></br>";
   }
 ?>
+
 <button class="getter">actualiser</button></br>
 
 <div class="the-return">
-  [HTML is replaced when successful.]
+  status
 </div>
 
 

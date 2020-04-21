6031d778b2023f97baed9a1bb5413e650f9cabd4
Gabriel Joulié <gabriel@gaggou.com>
2020-04-21 21:02:46 +0200

    deuxieme version, avec la lib paho-mqtt pour l'enregistrement mqtt

--- essais/mqtt_client.py
+++ essais/mqtt_client.py
@@ -2,22 +2,26 @@
 import sys
 import os
 import requests
+import paho.mqtt.subscribe as subscribe
+
 odd=True
 while True:
-  line = sys.stdin.readline()
+  line=subscribe.simple('/esp/sensor', hostname="localhost", retained=False).payload
+  #line = sys.stdin.readline().rstrip()
+  print('received {}\n'.format((line)))
   if line == '':
     exit(0)
-  if line == '449775a\n':
+  if line == '449775a':
     print os.popen("curl -X POST https://maker.ifttt.com/trigger/suspend_alarm/with/key/d9z64lBOBRi4KkrFduEYH5").read()
-  elif line == 'b218cf21\n':
+  elif line == 'b218cf21':
     print os.popen("curl -X POST https://maker.ifttt.com/trigger/arm_alarm/with/key/d9z64lBOBRi4KkrFduEYH5").read()
-  elif line == '439775a\n':
+  elif line == '439775a':
     if odd:
       requests.get('http://192.168.1.48/set?c=580001&m=0')
     else:
       requests.get('http://192.168.1.48/set?c=003d09&m=4')
     odd = not odd
-  elif line == '441775a\n':
+  elif line == '441775a':
     if odd:
       requests.get('http://192.168.1.48/set?m=33')
     else:

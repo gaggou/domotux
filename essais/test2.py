78f3d83c185cfa507034058d8869c175ad8ad269
Gabriel Joulié <gabriel@gaggou.com>
2020-04-23 18:28:04 +0200

    Refactor the example code into a callable class, with more states to save

--- essais/test2.py
+++ essais/test2.py
@@ -5,25 +5,37 @@ import requests
 import paho.mqtt.subscribe as subscribe
 
 
-def callback_user1(client, userdata, message):
-    if not hasattr(callback_user1, 'odd'):
-        callback_user1.odd = True
-    odd = callback_user1.odd = not callback_user1.odd
+class MessageHandler:
+  def __init__(self, verbose = False):
+    self.odd = True
+    self.verbose = verbose
+  def __call__(self, client, userdata, message):
+    self.odd = not self.odd
     line = message.payload
-    print('received "{}"'.format((line)))
+    if self.verbose:
+      print('received "{}"'.format((line)))
     if line == '439775a':
-      if odd:
-        requests.get('http://192.168.1.48/set?c=580001&m=0')
+      if self.odd:
+        self.request('http://192.168.1.48/set?c=580001&m=0')
       else:
-        requests.get('http://192.168.1.48/set?c=003d09&m=4')
+        self.request('http://192.168.1.48/set?c=003d09&m=4')
     elif line == '441775a':
-      if odd:
-        requests.get('http://192.168.1.48/set?m=33')
+      if self.odd:
+        self.request('http://192.168.1.48/set?m=33')
       else:
-        requests.get('http://192.168.1.48/set?m=39')
+        self.request('http://192.168.1.48/set?m=39')
+    elif line == 'toggleVerbose':
+      self.verbose = not self.verbose
+      if self.verbose:
+        print('Verbose on')
+  def request(self, url):
+    if self.verbose:
+      print('Request {}'.format(url))
+    requests.get(url)
 
 
 #line=subscribe.simple('/esp/sensor', hostname="localhost", retained=False).payload
-subscribe.callback(callback_user1, '/esp/sensor')
+on_call = MessageHandler()
+subscribe.callback(on_call, '/esp/sensor')
 
 
